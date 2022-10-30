d73377ac7fad7b577ea3b4ba2a67aa13c21c8b2e
imansedky5 <imans.ewaiti@gmail.com>
2022-10-30 10:28:43 +0200

    Create header component and style it

---
+++ figo/src/components/Header.js
@@ -0,0 +1,17 @@
+export const Header = () => {
+  return (
+    <header>
+      <div className="header-container">
+        <div className="intro">
+          <h1>We design interfaces that users love</h1>
+          <p>
+            Morbi sed lacus nec risus finibus feugiat et fermentum nibh.
+            Pellentesque vitae ante at elit fringilla ac at purus.
+          </p>
+          <button>Learn More</button>
+        </div>
+        <img src="./images/hero-banner.png"></img>
+      </div>
+    </header>
+  );
+};
